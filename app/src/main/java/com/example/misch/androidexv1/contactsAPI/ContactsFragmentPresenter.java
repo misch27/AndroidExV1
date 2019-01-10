c236c9c6ef4dbe77cfbe919f4125a41938f5a851
misch <misch27@ya.ru>
2019-01-10 13:19:51 +0300

    Удаление пустых импортов в классах

--- app/src/main/java/com/example/misch/androidexv1/contactsAPI/ContactsFragmentPresenter.java
+++ app/src/main/java/com/example/misch/androidexv1/contactsAPI/ContactsFragmentPresenter.java
@@ -5,17 +5,10 @@ import android.content.Context;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
-
-import com.example.misch.androidexv1.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class ContactsFragmentPresenter implements IContactsFragmentPresenter {
 
